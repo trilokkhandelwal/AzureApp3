61cc827ee30e5db45bead335124437da95e6e3e7
trilokkhandelwal <trilok.khandelwal@gmail.com>
2018-10-30 23:44:27 +0530

    Checkin

---
+++ AzureApp2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureApp2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
